02821020f6b1e38a93ab809248f3b29ca0e30184
Arnold0824 <418586403@qq.com>
2015-11-02 16:54:14 +0800

    fixed one bug

--- NineCo/views.py
+++ NineCo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
-from NiceCo.models import JobsInfo,Classification
+from NineCo.models import JobsInfo,Classification
 
 
 def Index(request):
