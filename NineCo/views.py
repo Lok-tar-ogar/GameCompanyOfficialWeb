b8df274eeea02450e14c109830edf3935323c014
Arnold0824 <418586403@qq.com>
2015-11-03 17:31:26 +0800

    almost there

--- NineCo/views.py
+++ NineCo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
-from NineCo.models import JobsInfo,Classification
+from NineCo.models import JobsInfo,Classification,GameInfo,GameClass
 
 
 def Index(request):
@@ -16,5 +16,15 @@ def contact(request):
 
 
 def jobs(request):
-	#jobsinfos=JobsInfo.objects.filter().order_by('dimDate')
-    return render_to_response("jobs.html")
+    jobsinfos=JobsInfo.objects.all().order_by('-dimDate')
+    classifications=Classification.objects.all()
+    return render_to_response("jobs.html",{'jb':jobsinfos,'cl':classifications})
+
+def gamelist(request):
+    games=GameInfo.objects.all().order_by('-dimDate')
+    return render_to_response("gamelist.html",{'gm':games})
+
+def gamecl(request):
+    games=GameInfo.objects.all().order_by('-dimDate')
+    gc=GameClass.objects.all()
+    return render_to_response("allgame.html",{'gm':games,'gc':gc})
