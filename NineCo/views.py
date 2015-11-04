864d12e047bd0a663136417d8eee7f629bb7eff8
dawsonrose <1768250331@qq.com>
2015-11-04 18:11:45 +0800

    news
    news what a beautiful day !

--- NineCo/views.py
+++ NineCo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
-from NineCo.models import JobsInfo,Classification,GameInfo,GameClass
+from NineCo.models import JobsInfo, Classification, GameInfo, GameClass, News
 
 
 def Index(request):
@@ -16,15 +16,22 @@ def contact(request):
 
 
 def jobs(request):
-    jobsinfos=JobsInfo.objects.all().order_by('-dimDate')
-    classifications=Classification.objects.all()
-    return render_to_response("jobs.html",{'jb':jobsinfos,'cl':classifications})
+    jobsinfos = JobsInfo.objects.all().order_by('-dimDate')
+    classifications = Classification.objects.all()
+    return render_to_response("jobs.html", {'jb': jobsinfos, 'cl': classifications})
+
 
 def gamelist(request):
-    games=GameInfo.objects.all().order_by('-dimDate')
-    return render_to_response("gamelist.html",{'gm':games})
+    games = GameInfo.objects.all().order_by('-dimDate')
+    return render_to_response("gamelist.html", {'gm': games})
+
 
 def gamecl(request):
-    games=GameInfo.objects.all().order_by('-dimDate')
-    gc=GameClass.objects.all()
-    return render_to_response("allgame.html",{'gm':games,'gc':gc})
+    games = GameInfo.objects.all().order_by('-dimDate')
+    gc = GameClass.objects.all()
+    return render_to_response("allgame.html", {'gm': games, 'gc': gc})
+
+
+def NewsPage(request):
+    news = News.objects.all().order_by('-dimDate')
+    return render_to_response('News.html', locals())
