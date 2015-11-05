ac1699d2977b4b372aa610bc4487da240c0861d6
dawsonrose <1768250331@qq.com>
2015-11-05 14:52:16 +0800

    1
    1

--- NineCo/views.py
+++ NineCo/views.py
@@ -32,6 +32,43 @@ def gamecl(request):
     return render_to_response("allgame.html", {'gm': games, 'gc': gc})
 
 
+PageCount = 3
+PAGERLEN = 8
+
+
 def NewsPage(request):
-    news = News.objects.all().order_by('-dimDate')
-    return render_to_response('News.html', locals())
+    try:
+        curpage = int(request.GET.get('curpage', '1'))
+        allpage = int(request.GET.get('allpage', '1'))
+        pagetype = str(request.GET.get('pagetype', ''))
+    except ValueError:
+        curpage = 1
+        allpage = 1
+        pagetype = 1
+    if pagetype == 'pagedown':
+        curpage += 1
+    elif pagetype == 'pageup':
+        curpage -= 1
+    elif pagetype == 'pageto':
+        pass
+    startpos = (curpage - 1) * PageCount
+    endpos = startpos + PageCount
+    posts = News.objects.all().order_by('-dimDate')[startpos:endpos]
+    if curpage == 1 and allpage == 1:
+        allNewsCount = News.objects.count()
+        allpage = allNewsCount // PageCount
+        remainPost = allNewsCount % PageCount
+        if remainPost > 0:
+            allpage += 1
+    pagelist = []  # below are the logic of pagination
+    if(allpage - curpage > PAGERLEN - 2):
+        for i in range(curpage - 1 - PAGERLEN // 2 if curpage - 1 - PAGERLEN // 2 > 0 else 0, curpage - 1 + PAGERLEN // 2):
+            pagelist.append(i + 1)
+            if len(pagelist) > PAGERLEN - 1:
+                break
+    else:
+        for i in range(curpage - 1 - PAGERLEN // 2, curpage - 1 + PAGERLEN // 2 if curpage - 1 + PAGERLEN // 2 < allpage else allpage):
+            pagelist.append(i + 1)
+            if len(pagelist) > PAGERLEN - 1:
+                break
+    return render_to_response('News.html', {'news': posts, 'allpage': allpage, 'borderpage': allpage - 3, 'pagelist': pagelist, 'curpage': curpage})
